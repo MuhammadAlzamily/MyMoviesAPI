bdec4b0a3e7828c013a629a1856de70ce35f5ca6
Windows 10 <Windows 10@DESKTOP-I8MKI0G>
2021-02-05 19:28:09 +0300

    initial commit

---
+++ MoviesAPI/Models/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
